69474c7f93f5aaae80644c7198c0e1b5adcc6230
Alex Psitos <apsitos08@gmail.com>
2021-12-10 12:36:26 -0500

    Add arrays

--- model_1.rb
+++ model_1.rb
@@ -7,3 +7,8 @@ difficulty_level = "Advanced"
 been_summited = true
 is_haunted = false
 is_treacherous = true
+
+# Variables that use Arrays:
+local_fauna = ["bear", "elk", "chipmunk"]
+ages_of_last_ten_hikers = [23, 12, 19, 56, 78, 34, 39, 28, 41, 43]
+park_rangers = ["Dave Brackmann"]
